395143fbc370c9eb1cc15abb160c9920da260830
Mingyang Zheng <zhengzmy@gmail.com>
2018-07-15 13:34:31 -0700

    Platform and Connectivity

--- app/app.module.ts
+++ app/app.module.ts
@@ -31,6 +31,7 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { FavoriteService } from './services/favorite.service';
 import { CouchbaseService } from './services/couchbase.service';
+import { PlatformService } from './services/platform.service';
 
 import { baseURL } from './shared/baseurl';
 
@@ -82,7 +83,8 @@ import { baseURL } from './shared/baseurl';
         PromotionService,
         LeaderService,
         FavoriteService,
-        CouchbaseService
+        CouchbaseService,
+        PlatformService
     ],
     schemas: [
         NO_ERRORS_SCHEMA
